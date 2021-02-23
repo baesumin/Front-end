ed1d4192fd6105e83e61d700b2a893cef1402b0a
SM <52240990+baesumin@users.noreply.github.com>
2021-02-23 09:47:56 +0900

    Create 0223(object).js

---
+++ javascript/0223(object).js
@@ -0,0 +1,8 @@
+var grades = {
+    'list' : {'egoing' : 10, 'k8805' : 8, 'sorialgi' : 80}
+    ,'show' : function(){
+        alert('Hellow world');
+    }
+}
+alert(grades['list']['egoing']);
+alert(grades['show']());
