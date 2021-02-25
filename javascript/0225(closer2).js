15efcc994a1fd7f1c3012dbe1df71ef3146b1253
SM <52240990+baesumin@users.noreply.github.com>
2021-02-25 11:28:37 +0900

    Create 0225(closer2).js

---
+++ javascript/0225(closer2).js
@@ -0,0 +1,19 @@
+var arr = []
+for(var i = 0; i < 5; i++){
+    arr[i] = function(id) {
+        return function(){
+            return id;
+        }
+    }(i);
+}
+for(var index in arr) {
+    console.log(arr[index]());
+}
+
+/* 결과
+0
+1
+2
+3
+4
+*/
