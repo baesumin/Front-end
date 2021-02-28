a400405be520884858a732d0bbbf1f726aed8c76
SM <52240990+baesumin@users.noreply.github.com>
2021-02-28 13:15:55 +0900

    Create 0228(Proto_rand).js

---
+++ javascript/0228(Proto_rand).js
@@ -0,0 +1,6 @@
+Array.prototype.rand = function(){
+    var index = Math.floor(this.length*Math.random());
+    return this[index];
+}
+var arr = new Array('seoul','new york','ladarkh','pusan', 'Tsukuba');
+console.log(arr.rand());
