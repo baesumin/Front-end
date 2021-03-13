0d19d49949c0f3d12ab9db0ce9b6e5289ea69d78
SM <52240990+baesumin@users.noreply.github.com>
2021-03-13 14:58:29 +0900

    update components

---
+++ react/jsx/src/index.js
@@ -0,0 +1,29 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function getButtonText(){
+    return 'Click on me!';
+}
+
+// Create a react component
+const App = () => {
+    const buttonText = ['Hi','There'];
+    return (
+    <div>
+        <label className="label" htmlFor="name">
+            Enter name:
+        </label>
+        <input id="name" type="text" />
+        <button style={{backgroundColor: 'blue', color:'white'}}>
+            {getButtonText()}
+        </button>
+    </div>
+    );
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+); 
