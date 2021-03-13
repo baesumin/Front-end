9c2190441ce5f55aaade537fd204936e17ecf111
SM <52240990+baesumin@users.noreply.github.com>
2021-03-13 20:47:04 +0900

    update components

---
+++ react/components/.eslintrc.js
@@ -0,0 +1,34 @@
+// module.exports = {
+// 	env: {
+// 		browser: true,
+// 		es2020: true,
+// 		node: true,
+// 	},
+// 	extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+// 	parserOptions: {
+// 		ecmaVersion: 11,
+// 		sourceType: 'module',
+// 		parser: 'babel-eslint',
+// 	},
+// 	plugins: ['prettier'],
+// 	rules: {
+// 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+// 		'no-unused-vars': 'off',
+// 		'vue/no-unused-components': 'off', // 미사용 변수 체크 해제,
+// 		'prettier/prettier': [
+// 			'error',
+// 			{
+// 				singleQuote: true,
+// 				semi: true,
+// 				useTabs: true,
+// 				tabWidth: 2,
+// 				trailingComma: 'all',
+// 				printWidth: 80,
+// 				bracketSpacing: true,
+// 				arrowParens: 'avoid',
+// 				endOfLine: 'auto',
+// 			},
+// 		],
+
+// 	},
+// };
--- react/components/src/index.js
+++ react/components/src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CommentDetail from './CommentDetail';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 4:45PM"
+                content="Nice blog post"
+            />
+            <CommentDetail
+                author="Alex"
+                timeAgo="Today at 2:00AM"
+                content="I like the subject"
+            />
+            <CommentDetail
+                author="Jane"
+                timeAgo="Today at 5:00PM"
+                content="I like the writing"
+            />
+        </div>
+    );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+// https://source.unsplash.com/random
