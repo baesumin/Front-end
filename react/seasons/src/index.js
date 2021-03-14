1f5e21ffd7e23e845b7b7b26c0559fbf0a08ccf8
SM <52240990+baesumin@users.noreply.github.com>
2021-03-14 13:14:45 +0900

    update seasons

---
+++ react/seasons/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// const App = () => {
+//     return <div>Latitude: </div>;
+// };
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            (err) => console.log(err)
+        );
+    }
+
+    render() {
+        return <div>Latitude: {this.state.lat}</div>;
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
