f05afda47df0ad0b1357cd51327dcdce0b17d834
SM <52240990+baesumin@users.noreply.github.com>
2021-03-16 17:22:56 +0900

    video

--- react/videos/src/components/App.js
+++ react/videos/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
     state = { videos: [] };
@@ -19,8 +20,8 @@ class App extends React.Component {
     render() {
         return (
             <div className="ui container">
-                <SearchBar onFormSubmit={this.onTermSubmit} />I
-                have {this.state.videos.length} videos.
+                <SearchBar onFormSubmit={this.onTermSubmit} />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
---
+++ react/videos/src/components/VideoItem.js
@@ -0,0 +1,15 @@
+import './VideoItem.css';
+import React from 'react';
+
+const VideoItem = ({ video }) => {
+    return (
+        <div className="video-item item">
+            <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+            <div className="content">
+                <div className="header">{video.snippet.title}</div>
+            </div>
+        </div>
+    );
+};
+
+export default VideoItem;
