e2aae4b735fd2a840d1a9338181bfcdc982aec86
SM <52240990+baesumin@users.noreply.github.com>
2021-03-18 18:01:19 +0900

    widgets

--- react/widgets/src/App.js
+++ react/widgets/src/App.js
@@ -4,6 +4,7 @@ import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
 import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
     {
@@ -63,6 +64,7 @@ export default () => {
     const [selected, setSelected] = useState(options[0]);
     return (
         <div>
+            <Header />
             <Route path="/">
                 <Accordion items={items} />
             </Route>
