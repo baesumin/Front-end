581a212062c2cc48b9d39b0f4328c2dfe2a0b89d
SM <52240990+baesumin@users.noreply.github.com>
2021-03-20 11:28:09 +0900

    usehooks

--- react/videos-hooks/src/components/App.js
+++ react/videos-hooks/src/components/App.js
@@ -1,32 +1,21 @@
 import React, { useState, useEffect } from 'react';
 
 import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
+import useVideos from '../hooks/useVideos';
 
 const App = () => {
-    const [videos, setVideos] = useState([]);
     const [selectedVideo, setSelectedVideo] = useState(null);
+    const [videos, search] = useVideos('youtube');
 
     useEffect(() => {
-        onTermSubmit('youtube');
-    }, []);
-
-    const onTermSubmit = async (term) => {
-        const response = await youtube.get('/search', {
-            params: {
-                q: term
-            }
-        });
-
-        setVideos(response.data.items);
-        setSelectedVideo(response.data.items[0]);
-    };
+        setSelectedVideo(videos[0]);
+    }, [videos]);
 
     return (
         <div className="ui container">
-            <SearchBar onFormSubmit={onTermSubmit} />
+            <SearchBar onFormSubmit={search} />
             <div className="ui grid">
                 <div className="ui row">
                     <div className="eleven wide column">
