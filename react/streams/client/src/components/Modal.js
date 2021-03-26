08d84337d7bc3c005f99e2d3c7c6384053d7ddc1
SM <52240990+baesumin@users.noreply.github.com>
2021-03-26 23:14:21 +0900

    modal

--- react/streams/client/src/components/Modal.js
+++ react/streams/client/src/components/Modal.js
@@ -5,10 +5,13 @@ import history from '../history';
 const Modal = (props) => {
     return ReactDOM.createPortal(
         <div
-            onClick={(e) => e.stopPropagation()}
+            onClick={props.onDismiss}
             className="ui dimmer modals visible active"
         >
-            <div className="ui standard modal visible active">
+            <div
+                onClick={(e) => e.stopPropagation()}
+                className="ui standard modal visible active"
+            >
                 <div className="header">{props.title}</div>
                 <div className="content">{props.content}</div>
                 <div className="actions">{props.actions}</div>
