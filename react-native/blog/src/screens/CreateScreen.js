e06715234406bfe74e2ee7413ff13ec87e9507ba
SM <52240990+baesumin@users.noreply.github.com>
2021-04-01 21:55:10 +0900

    Update CreateScreen.js

--- react-native/blog/src/screens/CreateScreen.js
+++ react-native/blog/src/screens/CreateScreen.js
@@ -1,11 +1,14 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
 
 const CreateScreen = ({ navigation }) => {
     return (
         <View>
-            <Text>Create</Text>
+            <Text>Enter Title:</Text>
+            <TextInput />
+            <Text>Enter Content:</Text>
+            <TextInput />
         </View>
     );
 };
