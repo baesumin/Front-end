5041c4c426dfb634a41f1d4c6f9ecf1a03a99c58
SM <52240990+baesumin@users.noreply.github.com>
2021-04-03 21:19:58 +0900

    mogodb

---
+++ react-native/track-server/src/index.js
@@ -0,0 +1,32 @@
+require('./model/User');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri =
+    'mongodb+srv://admin:admin@cluster0.wj5ah.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+    console.error('error connecting to mongo', err);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hi there!');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
---
+++ react-native/track-server/src/routes/authRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = new User({ email, password });
+        await user.save();
+
+        res.send('You made a post request');
+    } catch (err) {
+        return res.status(422).send(err.message);
+    }
+});
+
+module.exports = router;
