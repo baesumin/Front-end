b4ef13de46343cb5342c9b98c4d687b75aed3b1d
SM <52240990+baesumin@users.noreply.github.com>
2021-04-05 19:26:16 +0900

    track

--- react-native/track-server/src/index.js
+++ react-native/track-server/src/index.js
@@ -1,14 +1,17 @@
 require('./model/User');
+require('./model/Track');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri =
     'mongodb+srv://admin:admin@cluster0.wj5ah.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
