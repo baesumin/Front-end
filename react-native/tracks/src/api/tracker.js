e35118b2f099dc9794f5346a7fc564bd6a07544a
SM <52240990+baesumin@users.noreply.github.com>
2021-04-11 22:57:46 +0900

    Update tracker.js

--- react-native/tracks/src/api/tracker.js
+++ react-native/tracks/src/api/tracker.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
 
 export default axios.create({
-    baseURL: 'http://00cc91884f95.ngrok.io'
+    baseURL: 'http://b228ab14b2a3.ngrok.io'
 });
