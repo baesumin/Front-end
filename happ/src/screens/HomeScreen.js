eaeafbbd8665d27ebfafe731e64eb06c09f36c0c
SM <52240990+baesumin@users.noreply.github.com>
2021-04-15 23:29:04 +0900

    happ

--- happ/src/screens/HomeScreen.js
+++ happ/src/screens/HomeScreen.js
@@ -1,5 +1,20 @@
 import React from 'react';
-import { TodayHappy, Box } from './ScreenElements';
+import {
+    TodayHappyDiv,
+    Box,
+    TodayHappyContainer1,
+    TodayHappyContainer1Text,
+    TodayHappyContainer2,
+    TodayHappyContainer1Box,
+    MyCommunityDiv,
+    AttentionHappyDesignDiv,
+    CommonContainer1,
+    CommonContainer1Text,
+    CommonContainer1Box,
+    NoticeDiv
+} from './ScreenElements';
+import { BsQuestionCircle } from 'react-icons/bs';
+import { IconContext } from 'react-icons';
 
 const HomeScreen = () => {
     return (
@@ -9,8 +24,50 @@ const HomeScreen = () => {
                 src={process.env.PUBLIC_URL + '/mainUI.jpg'}
                 alt="copy url"
             />
-            <TodayHappy />
+            <TodayHappyDiv>
+                <TodayHappyContainer1>
+                    <TodayHappyContainer1Box />
+                    <TodayHappyContainer1Text>
+                        <p>#오늘 행복 어때?</p>★ 뽀기를 누르고 오른쪽으로 당겨보세요~ ★
+                    </TodayHappyContainer1Text>
+                    <TodayHappyContainer1Box>
+                        <IconContext.Provider value={{ size: '3rem', color: '#F26f81' }}>
+                            <div>
+                                <BsQuestionCircle style={{ cursor: 'pointer' }} />
+                            </div>
+                        </IconContext.Provider>
+                    </TodayHappyContainer1Box>
+                </TodayHappyContainer1>
+                <TodayHappyContainer2></TodayHappyContainer2>
+            </TodayHappyDiv>
             <Box />
+            <MyCommunityDiv>
+                <CommonContainer1>
+                    <CommonContainer1Text>나의 커뮤니티</CommonContainer1Text>
+                    <CommonContainer1Box>
+                        <IconContext.Provider value={{ size: '3rem', color: '#F26f81' }}>
+                            <div>
+                                <BsQuestionCircle style={{ cursor: 'pointer' }} />
+                            </div>
+                        </IconContext.Provider>
+                    </CommonContainer1Box>
+                </CommonContainer1>
+            </MyCommunityDiv>
+            <Box />
+            <AttentionHappyDesignDiv>
+                <CommonContainer1>
+                    <CommonContainer1Text>관심 행복 디자인</CommonContainer1Text>
+                    <CommonContainer1Box>
+                        <IconContext.Provider value={{ size: '3rem', color: '#F26f81' }}>
+                            <div>
+                                <BsQuestionCircle style={{ cursor: 'pointer' }} />
+                            </div>
+                        </IconContext.Provider>
+                    </CommonContainer1Box>
+                </CommonContainer1>
+            </AttentionHappyDesignDiv>
+            <Box />
+            <NoticeDiv></NoticeDiv>
         </div>
     );
 };
--- happ/src/screens/ScreenElements.js
+++ happ/src/screens/ScreenElements.js
@@ -14,8 +14,65 @@ export const Screen = styled.div`
     margin: 0px auto;
 `;
 //HomeScreen
-export const TodayHappy = styled.div`
+export const TodayHappyDiv = styled.div`
     height: 720px;
+    background-color: white;
+    margin: 0px auto;
+`;
+export const TodayHappyContainer1 = styled.div`
+    border: 1px solid;
+    display: flex;
+    padding-top: 30px;
+`;
+export const TodayHappyContainer1Text = styled.div`
+    text-align: center;
+    font-size: 1.8rem;
+    font-weight: bold;
+    line-height: 3rem;
+    width: 640px;
+    color: #999999;
+`;
+export const TodayHappyContainer1Box = styled.div`
+    border: 1px solid;
+    width: 80px;
+`;
+export const TodayHappyContainer2 = styled.div`
+    height: 300px;
+    border: 1px solid;
+    text-align: center;
+    font-size: 1.5rem;
+    padding-top: 15px;
+    //color: #eeeeee;
+`;
+export const MyCommunityDiv = styled.div`
+    height: 360px;
+    background-color: white;
+    margin: 0px auto;
+`;
+export const CommonContainer1 = styled.div`
+    border: 1px solid;
+    display: flex;
+    padding-top: 30px;
+`;
+export const CommonContainer1Text = styled.div`
+    border: 1px solid;
+    font-size: 2rem;
+    font-weight: bold;
+    padding-left: 30px;
+    padding-top: 30px;
+    width: 640px;
+`;
+export const CommonContainer1Box = styled.div`
+    border: 1px solid;
+    width: 80px;
+`;
+export const AttentionHappyDesignDiv = styled.div`
+    height: 360px;
+    background-color: white;
+    margin: 0px auto;
+`;
+export const NoticeDiv = styled.div`
+    height: 360px;
     background-color: white;
     margin: 0px auto;
 `;
