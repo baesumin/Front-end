2885ebaa682242cccc953a8c7f1c8bcf9520589d
SM <52240990+baesumin@users.noreply.github.com>
2021-04-20 18:32:36 +0900

    webproject

--- react/webproject/src/App.js
+++ react/webproject/src/App.js
@@ -2,37 +2,173 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Post from './components/Post';
-import { db } from './firebase';
+import { auth, db } from './firebase';
+import { Button, Input, Modal } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import ImageUpload from './components/ImageUpload';
+
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        position: 'absolute',
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3)
+    }
+}));
 
 function App() {
+    const classes = useStyles();
+    const [modalStyle] = useState(getModalStyle);
+
     const [posts, setPosts] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [openSignIn, setOpenSignIn] = useState(false);
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [user, setUser] = useState(null);
 
     useEffect(() => {
-        db.collection('posts').onSnapshot((snapshot) => {
-            setPosts(
-                snapshot.docs.map((doc) => ({
-                    id: doc.id,
-                    post: doc.data()
-                }))
-            );
+        const unsubcribe = auth.onAuthStateChanged((authUser) => {
+            if (authUser) {
+                console.log(authUser);
+                setUser(authUser);
+            } else {
+                setUser(null);
+            }
         });
+
+        return () => {
+            unsubcribe();
+        };
+    }, [user, username]);
+
+    useEffect(() => {
+        db.collection('posts')
+            .orderBy('timestamp', 'desc')
+            .onSnapshot((snapshot) => {
+                setPosts(
+                    snapshot.docs.map((doc) => ({
+                        id: doc.id,
+                        post: doc.data()
+                    }))
+                );
+            });
     }, []);
 
-    return (
-        <div className="App">
-            <Header />
-            {posts.length}
+    const signUp = (event) => {
+        event.preventDefault();
 
-            <Post
-                username="capcha"
-                caption="wow it works"
-                imageUrl="https://t1.daumcdn.net/cfile/blog/15450E415135F1A334"
-            />
-            <Post
-                username="capcha"
-                caption="wow it works"
-                imageUrl="https://t1.daumcdn.net/cfile/blog/15450E415135F1A334"
-            />
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((authUser) => {
+                authUser.user.updateProfile({
+                    displayName: username
+                });
+            })
+            .catch((error) => alert(error.message));
+
+        setOpen(false);
+    };
+
+    const signIn = (event) => {
+        event.preventDefault();
+
+        auth.signInWithEmailAndPassword(email, password).catch((error) =>
+            alert(error.message)
+        );
+
+        setOpenSignIn(false);
+    };
+
+    return (
+        <div className="app">
+            <Modal open={open} onClose={() => setOpen(false)}>
+                <div style={modalStyle} className={classes.paper}>
+                    <form className="app__signup">
+                        <center>
+                            <img
+                                className="app__headerImage"
+                                src="https://play-lh.googleusercontent.com/nYxL5NswAFjaxpwYsF3OzlKSfvfQI3wSf-8T9UT3qxuoTGsfGddhvOaHTxY7nU820NM"
+                                alt=""
+                            />
+                        </center>
+                        <Input
+                            placeholder="username"
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <Input
+                            placeholder="email"
+                            type="text"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <Input
+                            placeholder="password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <Button type="submit" onClick={signUp}>
+                            Login
+                        </Button>
+                    </form>
+                </div>
+            </Modal>
+            <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+                <div style={modalStyle} className={classes.paper}>
+                    <form className="app__signup">
+                        <center>
+                            <img
+                                className="app__headerImage"
+                                src="https://play-lh.googleusercontent.com/nYxL5NswAFjaxpwYsF3OzlKSfvfQI3wSf-8T9UT3qxuoTGsfGddhvOaHTxY7nU820NM"
+                                alt=""
+                            />
+                        </center>
+                        <Input
+                            placeholder="email"
+                            type="text"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <Input
+                            placeholder="password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <Button type="submit" onClick={signIn}>
+                            Login
+                        </Button>
+                    </form>
+                </div>
+            </Modal>
+
+            <Header />
+
+            {user ? (
+                <Button onClick={() => auth.signOut()}>Logout</Button>
+            ) : (
+                <div className="app__loginContainer">
+                    <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+                    <Button onClick={() => setOpen(true)}>Sign Up</Button>
+                </div>
+            )}
+
             {posts.map(({ id, post }) => (
                 <Post
                     key={id}
@@ -41,6 +177,12 @@ function App() {
                     imageUrl={post.imageUrl}
                 />
             ))}
+
+            {user?.displayName ? (
+                <ImageUpload username={user.displayName} />
+            ) : (
+                <h3>sorry you need to login to upload</h3>
+            )}
         </div>
     );
 }
