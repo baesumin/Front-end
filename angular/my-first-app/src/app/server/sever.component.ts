eb09dbee45386f6fb8c93797328d631954c725f6
SM <52240990+baesumin@users.noreply.github.com>
2021-05-25 13:31:42 +0900

    component

--- angular/my-first-app/src/app/server/sever.component.ts
+++ angular/my-first-app/src/app/server/sever.component.ts
@@ -4,4 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+}
