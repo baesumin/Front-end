d5c04931cbd4e68c13d3d47c47fb672c8a917c72
SM <52240990+baesumin@users.noreply.github.com>
2021-05-27 18:43:43 +0900

    list

--- angular/my-first-app/src/app/header/header.component.ts
+++ angular/my-first-app/src/app/header/header.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
   }
-
 }
--- angular/my-first-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ angular/my-first-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
