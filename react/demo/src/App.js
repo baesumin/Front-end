0da00b5598684571a692ad0faa74d9c9adb6adf9
baesumin <qotnals12345@naver.com>
2021-07-17 23:12:43 +0900

    update

--- react/demo/src/App.js
+++ react/demo/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Learn React Testing</h1>
+      <h1>Learn React Testing!!</h1>
     </div>
   );
 }
--- react/demo/src/App.test.js
+++ react/demo/src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+//import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import App from './App';
 
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // render(<App />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+  // throw new Error('Fail this test!');
+
+  const wrapper = shallow(<App />);
 });
