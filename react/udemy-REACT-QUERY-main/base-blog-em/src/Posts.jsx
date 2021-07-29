376eea7bac85f4930fb91951aeb2755b9dfe0cba
baesumin <qotnals12345@naver.com>
2021-07-29 20:08:27 +0900

    react query dev

--- react/udemy-REACT-QUERY-main/base-blog-em/src/Posts.jsx
+++ react/udemy-REACT-QUERY-main/base-blog-em/src/Posts.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 
-import { PostDetail } from "./PostDetail";
+import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
 
 async function fetchPosts() {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0'
   );
   return response.json();
 }
@@ -15,17 +16,21 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+  const { data, isError, error, isLoading } = useQuery('posts', fetchPosts);
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError)
+    return (
+      <>
+        <h3>Oops, something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
       <ul>
         {data.map((post) => (
-          <li
-            key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
+          <li key={post.id} className="post-title" onClick={() => setSelectedPost(post)}>
             {post.title}
           </li>
         ))}
