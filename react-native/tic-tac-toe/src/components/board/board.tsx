e78a1deb7ba17721a6ac644ff50aea5cfcf15876
baesumin <qotnals12345@naver.com>
2021-08-10 22:15:55 +0900

    debug

---
+++ react-native/tic-tac-toe/src/components/board/board.tsx
@@ -0,0 +1,44 @@
+import React, { ReactElement } from 'react';
+import { View, TouchableOpacity } from 'react-native';
+
+import Text from '../text/text';
+import { BoardState } from '@utils';
+
+type BoardProps = {
+  state: BoardState;
+  size: number;
+  onCellPressed?: (index: number) => void;
+};
+
+export default function Board({ state, size, onCellPressed }: BoardProps): ReactElement {
+  return (
+    <View
+      style={{
+        width: size,
+        height: size,
+        backgroundColor: 'green',
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+      }}
+    >
+      {state.map((cell, index) => {
+        return (
+          <TouchableOpacity
+            onPress={() => onCellPressed && onCellPressed(index)}
+            style={{
+              width: '33.33333%',
+              height: '33.33333%',
+              backgroundColor: '#fff',
+              borderWidth: 1,
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+            key={index}
+          >
+            <Text style={{ fontSize: size / 8 }}>{cell}</Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
--- react-native/tic-tac-toe/src/screens/single-player-game/single-player-game.tsx
+++ react-native/tic-tac-toe/src/screens/single-player-game/single-player-game.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import { View, Text } from 'react-native';
 
 import { GradientBackground } from '@components';
 import styles from './single-player-game.styles';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { ReactElement } from 'react';
+import { Board } from '@components';
+import { printFormattedBoard, BoardState } from '@utils';
 
 export default function Game(): ReactElement {
+  const b: BoardState = ['x', 'o', null, 'x', 'o', null, 'x', 'o', null];
+  printFormattedBoard(b);
   return (
     <GradientBackground>
       <SafeAreaView style={styles.container}>
-        <Text style={{ color: '#fff' }}>Game</Text>
+        <Board
+          onCellPressed={(index) => {
+            alert(index);
+          }}
+          state={b}
+          size={300}
+        />
       </SafeAreaView>
     </GradientBackground>
   );
