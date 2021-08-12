b8be3f1e7343c51bf998b9ec59283d4303f14245
baesumin <qotnals12345@naver.com>
2021-08-12 11:45:08 +0900

    sounds

--- react-native/tic-tac-toe/src/screens/single-player-game/single-player-game.tsx
+++ react-native/tic-tac-toe/src/screens/single-player-game/single-player-game.tsx
@@ -1,9 +1,12 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState, useEffect, useRef } from 'react';
 import { View, Text } from 'react-native';
 
 import { GradientBackground } from '@components';
 import styles from './single-player-game.styles';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Audio } from 'expo-av';
+import * as Haptics from 'expo-haptics';
+
 import { Board } from '@components';
 import {
   printFormattedBoard,
@@ -14,28 +17,92 @@ import {
   isTerminal,
   getBestMove
 } from '@utils';
-import { useState } from 'react';
 
 export default function Game(): ReactElement {
   // prettier-ignore
   const [state, setState] = useState<BoardState>([
-    null, 'x', null,
-    'o', null, 'x',
-    'o', 'o', 'x',
+    null, null, null,
+    null, null, null,
+    null, null, null,
   ])
-  console.log('getBestMove', getBestMove(state, true));
+  const [turn, setTurn] = useState<'HUMAN' | 'BOT'>(
+    Math.random() < 0.5 ? 'HUMAN' : 'BOT'
+  );
+  const [isHumanMaximizing, setIsHumanMaximizing] = useState<boolean>(true);
+  const popSoundRef = useRef<Audio.Sound | null>(null);
+  const pop2SoundRef = useRef<Audio.Sound | null>(null);
 
-  const handleOnCellPressed = (cell: number): void => {
+  const gameResult = isTerminal(state);
+  // console.log('getBestMove', getBestMove(state, true));
+
+  const insertCell = (cell: number, symbol: 'x' | 'o'): void => {
     const stateCopy: BoardState = [...state];
     if (stateCopy[cell] || isTerminal(stateCopy)) return;
-    stateCopy[cell] = 'x';
+    stateCopy[cell] = symbol;
     setState(stateCopy);
+    try {
+      symbol === 'x'
+        ? popSoundRef.current?.replayAsync()
+        : pop2SoundRef.current?.replayAsync();
+      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  const handleOnCellPressed = (cell: number): void => {
+    if (turn !== 'HUMAN') return;
+    insertCell(cell, isHumanMaximizing ? 'x' : 'o');
+    setTurn('BOT');
+  };
+
+  useEffect(() => {
+    if (gameResult) {
+      // handle game finished
+      alert('Game Over');
+    } else {
+      if (turn === 'BOT') {
+        if (isEmpty(state)) {
+          const centerAndCorners = [0, 2, 6, 8, 4];
+          const firstMove =
+            centerAndCorners[Math.floor(Math.random() * centerAndCorners.length)];
+          insertCell(firstMove, 'x');
+          setIsHumanMaximizing(false);
+          setTurn('HUMAN');
+        } else {
+          const best = getBestMove(state, !isHumanMaximizing, 0, -1);
+          insertCell(best, isHumanMaximizing ? 'o' : 'x');
+          setTurn('HUMAN');
+        }
+      }
+    }
+  }, [state, turn]);
+
+  useEffect(() => {
+    //load sounds
+    const popSoundObject = new Audio.Sound();
+    const pop2SoundObject = new Audio.Sound();
+
+    const loadSounds = async () => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      await popSoundObject.loadAsync(require('@assets/assets_pop_1.wav'));
+      popSoundRef.current = popSoundObject;
+      await pop2SoundObject.loadAsync(require('@assets/assets_pop_2.wav'));
+      pop2SoundRef.current = pop2SoundObject;
+    };
+    loadSounds();
+    return () => {
+      //unload sounds
+      popSoundObject && popSoundObject.unloadAsync();
+      pop2SoundObject && pop2SoundObject.unloadAsync();
+    };
+  }, []);
+
   return (
     <GradientBackground>
       <SafeAreaView style={styles.container}>
         <Board
-          disabled={Boolean(isTerminal(state))}
+          disabled={Boolean(isTerminal(state)) || turn === 'BOT'}
           onCellPressed={(index) => {
             handleOnCellPressed(index);
           }}
