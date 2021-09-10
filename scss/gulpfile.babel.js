ac09efd77c71136751b32f76b8d49a7202c3b20c
baesumin <qotnals12345@naver.com>
2021-09-10 14:06:34 +0900

    upate

--- scss/gulpfile.babel.js
+++ scss/gulpfile.babel.js
@@ -1,27 +1,27 @@
-import gulp from "gulp";
-import del from "del";
-import sass from "gulp-sass";
-import minify from "gulp-csso";
-import autoprefixer from "gulp-autoprefixer";
+import gulp from 'gulp';
+import del from 'del';
+import sass from 'gulp-sass';
+import minify from 'gulp-csso';
+import autoprefixer from 'gulp-autoprefixer';
 
-sass.compiler = require("node-sass");
+sass.compiler = require('node-sass');
 
 const routes = {
   css: {
-    watch: "src/scss/*",
-    src: "src/scss/styles.scss",
-    dest: "dist/css"
+    watch: 'src/scss/*',
+    src: 'src/scss/styles.scss',
+    dest: 'dist/css'
   }
 };
 
 const styles = () =>
   gulp
     .src(routes.css.src)
-    .pipe(sass().on("error", sass.logError))
+    .pipe(sass().on('error', sass.logError))
     .pipe(
       autoprefixer({
         flexbox: true,
-        grid: "autoplace"
+        grid: 'autoplace'
       })
     )
     .pipe(minify())
@@ -31,7 +31,7 @@ const watch = () => {
   gulp.watch(routes.css.watch, styles);
 };
 
-const clean = () => del(["dist/styles.css"]);
+const clean = () => del(['dist/styles.css']);
 
 const prepare = gulp.series([clean]);
 
