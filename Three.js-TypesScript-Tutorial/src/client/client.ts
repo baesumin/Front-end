8ff07c5856bf9b3ad76cfbb743c6f6c82dceb635
baesumin <qotnals12345@naver.com>
2021-09-28 17:19:18 +0900

    threejs with typescript

--- Three.js-TypesScript-Tutorial/src/client/client.ts
+++ Three.js-TypesScript-Tutorial/src/client/client.ts
@@ -1,6 +1,10 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import Stats from 'three/examples/jsm/libs/stats.module';
+import { GUI } from 'three/examples/jsm/libs/dat.gui.module';
 
 const scene = new THREE.Scene();
+scene.add(new THREE.AxesHelper(5));
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -8,20 +12,45 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.z = 2;
+camera.position.z = 3;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
+new OrbitControls(camera, renderer.domElement);
+
+const boxGeometry = new THREE.BoxGeometry();
+const sphereGeometry = new THREE.SphereGeometry();
+const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0);
+const planeGeometry = new THREE.PlaneGeometry();
+const torusKnotGeometry = new THREE.TorusKnotGeometry();
+
 const material = new THREE.MeshBasicMaterial({
   color: 0x00ff00,
   wireframe: true
 });
+//const material= new THREE.MeshNormalMaterial()
 
-const cube = new THREE.Mesh(geometry, material);
+const cube = new THREE.Mesh(boxGeometry, material);
+cube.position.x = 5;
 scene.add(cube);
+
+const sphere = new THREE.Mesh(sphereGeometry, material);
+sphere.position.x = 3;
+scene.add(sphere);
+
+const icosahedron = new THREE.Mesh(icosahedronGeometry, material);
+icosahedron.position.x = 0;
+scene.add(icosahedron);
+
+const plane = new THREE.Mesh(planeGeometry, material);
+plane.position.x = -2;
+scene.add(plane);
+
+const torusKnot = new THREE.Mesh(torusKnotGeometry, material);
+torusKnot.position.x = -5;
+scene.add(torusKnot);
 
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
@@ -31,13 +60,39 @@ function onWindowResize() {
   render();
 }
 
+const stats = Stats();
+document.body.appendChild(stats.dom);
+
+// const options = {
+//     side: {
+//         "FrontSide": THREE.FrontSide,
+//         "BackSide": THREE.BackSide,
+//         "DoubleSide": THREE.DoubleSide,
+//     }
+// }
+
+const gui = new GUI();
+const materialFolder = gui.addFolder('THREE.Material');
+// materialFolder.add(material, 'transparent')
+// materialFolder.add(material, 'opacity', 0, 1, 0.01)
+// materialFolder.add(material, 'depthTest')
+// materialFolder.add(material, 'depthWrite')
+// materialFolder.add(material, 'alphaTest', 0, 1, 0.01).onChange(() => updateMaterial())
+// materialFolder.add(material, 'visible')
+// materialFolder.add(material, 'side', options.side).onChange(() => updateMaterial())
+materialFolder.open();
+
+// function updateMaterial() {
+//     material.side = Number(material.side)
+//     material.needsUpdate = true
+// }
+
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
   render();
+
+  stats.update();
 }
 
 function render() {
