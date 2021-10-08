4305d154f0c16578ed54ab2d2fa550ccdc593d75
baesumin <qotnals12345@naver.com>
2021-10-08 17:22:01 +0900

    init

---
+++ react-native/langApp/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { Animated } from 'react-native';
+import styled from 'styled-components';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled.TouchableOpacity`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+const AnimatedBox = Animated.createAnimatedComponent(Box);
+
+export default function App() {
+  const Y = new Animated.Value(0);
+
+  const moveUp = () => {
+    Animated.timing(Y, {
+      toValue: 200,
+      useNativeDriver: true
+    }).start();
+  };
+
+  return (
+    <Container>
+      <AnimatedBox
+        onPress={moveUp}
+        style={{
+          transform: [{ translateY: Y }]
+        }}
+      />
+    </Container>
+  );
+}
