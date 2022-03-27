eaef86d050ac39762221a38f1ca14d1873fca1b3
baesumin <qotnals12345@naver.com>
2022-03-27 22:07:14 +0900

    update

--- nextjs/carrot-market/pages/api/products/[id].ts
+++ nextjs/carrot-market/pages/api/products/[id].ts
@@ -19,7 +19,22 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
       }
     }
   });
-  res.json({ ok: true, product });
+  const terms = product?.name.split(' ').map((word) => ({
+    name: {
+      contains: word
+    }
+  }));
+  const relatedProducts = await client.product.findMany({
+    where: {
+      OR: terms,
+      AND: {
+        id: {
+          not: product?.id
+        }
+      }
+    }
+  });
+  res.json({ ok: true, product, relatedProducts });
 }
 
 export default withApiSession(
--- nextjs/carrot-market/pages/products/[id].tsx
+++ nextjs/carrot-market/pages/products/[id].tsx
@@ -4,12 +4,22 @@ import Layout from '@components/layout';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import Link from 'next/link';
+import { Product, User } from '@prisma/client';
 
-
+interface ProductWithUser extends Product {
+  user: User;
+}
+interface ItemDetailResponse {
+  ok: boolean;
+  product: ProductWithUser;
+  relatedProducts: Product[];
+}
 
 const ItemDetail: NextPage = () => {
   const router = useRouter();
-  const { data } = useSWR(router.query.id ? `/api/products/${router.query.id}` : null);
+  const { data } = useSWR<ItemDetailResponse>(
+    router.query.id ? `/api/products/${router.query.id}` : null
+  );
   console.log(data);
 
   return (
@@ -61,11 +71,13 @@ const ItemDetail: NextPage = () => {
         <div>
           <h2 className="text-2xl font-bold text-gray-900">Similar items</h2>
           <div className=" mt-6 grid grid-cols-2 gap-4">
-            {[1, 2, 3, 4, 5, 6].map((_, i) => (
-              <div key={i}>
+            {data?.relatedProducts.map((product) => (
+              <div key={product.id}>
                 <div className="mb-4 h-56 w-full bg-slate-300" />
-                <h3 className="-mb-1 text-gray-700">Galaxy S60</h3>
-                <span className="text-sm font-medium text-gray-900">$6</span>
+                <h3 className="-mb-1 text-gray-700">{product.name}</h3>
+                <span className="text-sm font-medium text-gray-900">
+                  ${product.price}
+                </span>
               </div>
             ))}
           </div>
