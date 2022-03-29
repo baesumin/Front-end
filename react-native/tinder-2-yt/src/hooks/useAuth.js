54be3bc27dc3e826b56417e0888f5dcc19d11e04
baesumin <qotnals12345@naver.com>
2022-03-29 22:35:56 +0900

    update

--- react-native/tinder-2-yt/src/hooks/useAuth.js
+++ react-native/tinder-2-yt/src/hooks/useAuth.js
@@ -4,6 +4,10 @@ import * as Google from 'expo-google-app-auth';
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
+  const signInWithGoogle = async () => {
+    await Google.logInAsync();
+  };
+
   return (
     <AuthContext.Provider
       value={{
