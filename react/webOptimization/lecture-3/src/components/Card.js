a4e88eccda34fb0033484694a08af534f247dea5
baesumin <qotnals12345@naver.com>
2022-04-12 21:06:55 +0900

    practice

---
+++ react/webOptimization/lecture-3/src/components/Card.js
@@ -0,0 +1,32 @@
+import React, { useRef, useEffect } from 'react';
+
+function Card(props) {
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    const callback = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log('hi');
+          entry.target.src = entry.target.dataset.src;
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+    const options = {};
+    const observer = new IntersectionObserver(callback, options);
+
+    observer.observe(imgRef.current);
+  }, []);
+
+  return (
+    <div className="Card text-center">
+      <img data-src={props.image} ref={imgRef} />
+      <div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
+        {props.children}
+      </div>
+    </div>
+  );
+}
+
+export default Card;
