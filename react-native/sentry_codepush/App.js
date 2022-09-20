008b74ccd78aa3417abf4e26c13300fd310e62bb
baesumin <qotnals12345@naver.com>
2022-09-20 13:25:58 +0900

    코드푸시, 센트리 적용 확인

--- react-native/sentry_codepush/App.js
+++ react-native/sentry_codepush/App.js
@@ -1,5 +1,5 @@
-import React, {useEffect} from 'react';
-import {Button, SafeAreaView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Button, Image, SafeAreaView, Text} from 'react-native';
 import {DSN} from 'react-native-dotenv';
 import * as Sentry from '@sentry/react-native';
 import CodePush from 'react-native-code-push';
@@ -12,42 +12,58 @@ Sentry.init({
 });
 
 const codePushOptions = {
-  checkFrequency: CodePush.CheckFrequency.MANUAL,
+  // checkFrequency: CodePush.CheckFrequency.MANUAL,
+  checkFrequency: CodePush.CheckFrequency.ON_APP_START,
   // 언제 업데이트를 체크하고 반영할지를 정한다.
   // ON_APP_RESUME은 Background에서 Foreground로 오는 것을 의미
   // ON_APP_START은 앱이 실행되는(켜지는) 순간을 의미
 
-  installMode: CodePush.InstallMode.IMMEDIATE,
-  mandatoryInstallMode: CodePush.InstallMode.IMMEDIATE,
+  // installMode: CodePush.InstallMode.IMMEDIATE,
+  installMode: CodePush.InstallMode.ON_NEXT_RESTART,
+  // mandatoryInstallMode: CodePush.InstallMode.IMMEDIATE,
+  mandatoryInstallMode: CodePush.InstallMode.ON_NEXT_RESTART,
   // 업데이트를 어떻게 설치할 것인지 (IMMEDIATE는 강제설치를 의미)
 };
 
 const App = () => {
+  const [state, setState] = useState(0);
   const onClickButton = () => {
     console.log('hi');
     throw new Error('My first Sentry error!');
   };
 
   useEffect(() => {
+    // CodePush.checkForUpdate()
+    //   .then(update => {
+    //     if (update) {
+    //       Sentry.setRelease(update.appVersion + '-codepush:' + update.label); // <- this
+    //       CodePush.sync({
+    //         updateDialog: false,
+    //         installMode: CodePush.InstallMode.IMMEDIATE,
+    //       });
+    //     }
+    //   })
+    //   .catch(console.error);
     CodePush.sync(
       {
-        installMode: CodePush.InstallMode.IMMEDIATE,
-        mandatoryInstallMode: CodePush.InstallMode.IMMEDIATE,
-        updateDialog: {
-          mandatoryUpdateMessage:
-            '필수 업데이트가 있어 설치 후 앱을 재시작합니다.',
-          mandatoryContinueButtonLabel: '재시작',
-          optionalIgnoreButtonLabel: '나중에',
-          optionalInstallButtonLabel: '재시작',
-          optionalUpdateMessage: '업데이트가 있습니다. 설치하시겠습니까?',
-          title: '업데이트 안내',
-        },
+        installMode: CodePush.InstallMode.ON_NEXT_RESTART,
+        mandatoryInstallMode: CodePush.InstallMode.ON_NEXT_RESTART,
+        // updateDialog: {
+        //   mandatoryUpdateMessage:
+        //     '필수 업데이트가 있어 설치 후 앱을 재시작합니다.',
+        //   mandatoryContinueButtonLabel: '재시작',
+        //   optionalIgnoreButtonLabel: '나중에',
+        //   optionalInstallButtonLabel: '재시작',
+        //   optionalUpdateMessage: '업데이트가 있습니다. 설치하시겠습니까?',
+        //   title: '업데이트 안내',
+        // },
       },
       status => {
         console.log(`Changed ${status}`);
       },
-      downloadProgress => {
+      ({receivedBytes, totalBytes}) => {
         // 여기서 몇 % 다운로드되었는지 체크 가능
+        setState(receivedBytes);
       },
     ).then(status => {
       console.log(`CodePush ${status}`);
@@ -55,11 +71,36 @@ const App = () => {
   }, []);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{backgroundColor: 'white', flex: 1}}>
       <Button title="sentry" onPress={onClickButton} />
+      <Text style={{backgroundColor: 'pink'}}>
+        코드푸시로 생성한 텍스트이다 드디어 퍼센트를 볼 수 있다. 자연스럽게
+        업데이트되나 새로운 텍스트
+      </Text>
+      <Image
+        source={require('./kakao.jpg')}
+        style={{width: 200, height: 200}}
+      />
+      <Image
+        source={{
+          uri: 'https://t1.daumcdn.net/cfile/tistory/24283C3858F778CA2E',
+        }}
+        style={{width: 200, height: 200}}
+      />
+      <Image
+        source={{
+          uri: 'https://t1.daumcdn.net/cfile/tistory/24283C3858F778CA2E',
+        }}
+        style={{width: 200, height: 200}}
+      />
+      <Text style={{color: 'black', alignSelf: 'center'}}>{state}</Text>
+      <Text style={{color: 'black', alignSelf: 'center'}}>
+        스테이징 먼저 테스트후 프로덕션빌드 테스트
+      </Text>
     </SafeAreaView>
   );
 };
 
-export default CodePush(codePushOptions)(Sentry.wrap(App));
+export default CodePush(Sentry.wrap(App));
+// export default CodePush(codePushOptions)(Sentry.wrap(App));
 // export default CodePush(codePushOptions)(App);
