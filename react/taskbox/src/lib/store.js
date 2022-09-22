975491e2c6482aaaec47542d28b16dc2859223a9
baesumin <qotnals12345@naver.com>
2022-09-22 09:21:37 +0900

    storybook window

--- react/taskbox/src/lib/store.js
+++ react/taskbox/src/lib/store.js
@@ -1,7 +1,7 @@
 /* A simple redux store/actions/reducer implementation.
  * A true app would be more complex and separated into different files.
  */
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore, createSlice,createAsyncThunk } from '@reduxjs/toolkit';
 
 /*
  * The initial state of our store when the app loads.
@@ -20,6 +20,24 @@ const TaskBoxData = {
 };
 
 /*
+ * Creates an asyncThunk to fetch tasks from a remote endpoint.
+ * You can read more about Redux Toolkit's thunks in the docs:
+ * https://redux-toolkit.js.org/api/createAsyncThunk
+ */
+export const fetchTasks = createAsyncThunk('todos/fetchTodos', async () => {
+  const response = await fetch(
+    'https://jsonplaceholder.typicode.com/todos?userId=1'
+  );
+  const data = await response.json();
+  const result = data.map((task) => ({
+    id: `${task.id}`,
+    title: task.title,
+    state: task.completed ? 'TASK_ARCHIVED' : 'TASK_INBOX',
+  }));
+  return result;
+});
+
+/*
  * The store is created here.
  * You can read more about Redux Toolkit's slices in the docs:
  * https://redux-toolkit.js.org/api/createSlice
@@ -35,6 +53,29 @@ const TasksSlice = createSlice({
         state.tasks[task].state = newTaskState;
       }
     },
+  },
+  /*
+   * Extends the reducer for the async actions
+   * You can read more about it at https://redux-toolkit.js.org/api/createAsyncThunk
+   */
+  extraReducers(builder) {
+    builder
+    .addCase(fetchTasks.pending, (state) => {
+      state.status = 'loading';
+      state.error = null;
+      state.tasks = [];
+    })
+    .addCase(fetchTasks.fulfilled, (state, action) => {
+      state.status = 'succeeded';
+      state.error = null;
+      // Add any fetched tasks to the array
+      state.tasks = action.payload;
+      })
+    .addCase(fetchTasks.rejected, (state) => {
+      state.status = 'failed';
+      state.error = "Something went wrong";
+      state.tasks = [];
+    });
   },
 });
 
