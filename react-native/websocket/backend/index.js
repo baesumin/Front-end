73b6ae4a43ed9fad044c91c62c77300723b35c0a
baesumin <qotnals12345@naver.com>
2022-10-21 19:39:50 +0900

    init

---
+++ react-native/websocket/backend/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const WebSocket = require('ws');
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+const port = 3001;
