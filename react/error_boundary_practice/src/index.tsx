ce152d88c0d28b23dd62b5d656a681998093e6ee
Your Name <52240990+baesumin@users.noreply.github.com>
2022-12-21 22:50:02 +0900

    update

--- react/error_boundary_practice/src/index.tsx
+++ react/error_boundary_practice/src/index.tsx
@@ -1,11 +1,17 @@
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Router';
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
