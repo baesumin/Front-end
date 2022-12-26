9895ad5fc9f035c1a6bef1cfb64fad509d40d40a
Your Name <52240990+baesumin@users.noreply.github.com>
2022-12-26 23:45:54 +0900

    Update Header.tsx

--- react/error_boundary_practice/src/components/Header.tsx
+++ react/error_boundary_practice/src/components/Header.tsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 export default function Header() {
@@ -6,6 +8,37 @@ export default function Header() {
   const onAboutClick = () => {
     navigate('/about');
   };
+
+  useEffect(() => {
+    const formData = new FormData();
+    formData.append(
+      'request',
+      new Blob([JSON.stringify({ nickName: 'asd' })], { type: 'application/json' })
+    );
+    // fetch('http://13.125.60.187/user/nickname', {
+    //   headers: {
+    //     Authorization:
+    //       'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJHT09HTEVfMTE0MDE4MjUxODMyMTYzNzEyNjEyIiwicm9sZXMiOiJST0xFX1VTRVIiLCJpYXQiOjE2NzIwMzMzMjEsImV4cCI6MTY3MjAzNjkyMX0.dDcIRKVm6lC4yuyBDIOnx1xVXwIEmBdTbhBX6rsI-54',
+    //     'Content-Type': 'multipart/form-data'
+    //   },
+    //   method: 'post',
+    //   body: formData
+    // })
+    //   .then((res) => res.json())
+    //   .then((res) => console.log(res));
+
+    axios
+      .post('http://13.125.60.187/user/nickname', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization:
+            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJHT09HTEVfMTE0MDE4MjUxODMyMTYzNzEyNjEyIiwicm9sZXMiOiJST0xFX1VTRVIiLCJpYXQiOjE2NzIwMzMzMjEsImV4cCI6MTY3MjAzNjkyMX0.dDcIRKVm6lC4yuyBDIOnx1xVXwIEmBdTbhBX6rsI-54'
+        }
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  }, []);
 
   return (
     <header>
